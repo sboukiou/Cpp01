18210127d98a19b257ebbedd470d0bba0696bfdd
sboukiou <sboukiou@student.1337.ma>
2025-11-13 19:34:15 +0100

    HarlFilter done, used the first char as a value input for the switch statement

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,47 +6,56 @@ Harl::Harl() {}
 Harl::~Harl() {}
 
 void	Harl::debug( void ) {
+	std::cout << "[DEBUG]:\n";
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-\
-triple-pickle-specialketchup burger. I really do!";
+triple-pickle-specialketchup burger. I really do!\n";
 }
 
 void Harl::info( void ) {
+	std::cout << "[INFO]:\n";
 	std::cout << "I cannot believe adding extra bacon costs more money. \
 You didn’t put\
 enough bacon in my burger! \
-If you did, I wouldn’t be asking for more!";
+If you did, I wouldn’t be asking for more!\n";
 }
 
 void Harl::warning( void ) {
+	std::cout << "[WARNING]:\n";
 	std::cout << "I think I deserve to have some extra bacon for free. \
 I’ve been coming for\
-years, whereas you started working here just last month.";
+years, whereas you started working here just last month.\n";
 }
 
 void Harl::error( void ) {
-	std::cout << "This is unacceptable! I want to speak to the manager now.";
+	std::cout << "[ERROR]:\n";
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
 }
 
 void Harl::complain(std::string level) {
-	std::string levels[LVL_SIZE];
-	void (Harl::*fptr[LVL_SIZE])();
-
-	levels[0] = "DEBUG";
-	levels[1] = "INFO";
-	levels[2] = "WARNING";
-	levels[3] = "ERROR";
-
-	fptr[0] = &Harl::debug;
-	fptr[1] = &Harl::info;
-	fptr[2] = &Harl::warning;
-	fptr[3] = &Harl::error;
+	int	idx = 0;
 
 
-	for (int i = 0; i < LVL_SIZE;i += 1) {
-		if (level == levels[i]) {
-			(this->*fptr[i])();
-			return ;
-		}
+
+	switch (level[0]) {
+		case 'D':
+			Harl::debug();
+			Harl::info();
+			Harl::warning();
+			Harl::error();
+			break;
+		case 'I':
+			Harl::info();
+			Harl::warning();
+			Harl::error();
+			break;
+		case 'W':
+			Harl::warning();
+			Harl::error();
+			break;
+		case 'E':
+			Harl::error();
+			break;
+		default:
+			break;
 	}
-	std::cerr << "The given level is invalid\n";
 }
--- ex06/main.cpp
+++ ex06/main.cpp
@@ -1,19 +1,34 @@
 #include "Harl.hpp"
 #include <iostream>
 
-int main (void)
-{
-	Harl harl;
+static bool	isLevelValid(std::string level) {
+	if (level == "DEBUG")
+		return (true);
+	if (level == "INFO")
+		return (true);
+	if (level == "WARNING")
+		return (true);
+	if (level == "ERROR")
+		return (true);
+	return (false);
+}
 
-	std::cout << std::endl << "------ DEBUG Level ------" << std::endl;
-	harl.complain("DEBUG");
-	std::cout << std::endl << "------ INFO Level ------" << std::endl;;
-	harl.complain("INFO");
-	std::cout << std::endl << "------ WARNING Level ------" << std::endl;
-	harl.complain("WARNING");
-	std::cout << std::endl << "------ ERROR Level ------" << std::endl;
-	harl.complain("ERROR");
-	std::cout << std::endl << "------ No valid Level ------" << std::endl;
-	harl.complain("ALARM");
+int main ( int ac, char *av[] ) {
+
+	std::string level;
+	Harl		harl;
+
+	if (ac != 2) {
+		std::cerr << "[Error]: Number of agruments given\
+			is wrong\n";
+		return (1);
+	}
+	level = av[1];
+	if (isLevelValid(level) == false) {
+		std::cerr << "[Erro]: Invalid level given !\n";
+		return (1);
+	}
+
+	harl.complain(level);
 	return (0);
 }
