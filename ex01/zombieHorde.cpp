7437837b8b03e49bf1c7a5a621980f07dfc0df1b
sboukiou <sboukiou@student.1337.ma>
2025-11-14 20:53:32 +0100

    Added more checks for the null allocation

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -1,15 +1,20 @@
 #include "./Zombie.hpp"
+#include <iostream>
 
 Zombie* zombieHorde( int N, std::string name ) {
 
 	Zombie *collection;
 
-	if ( N == 0 )
+	if ( N == 0 ) {
+		std::cerr << "Number given is Null, No zombies are created!\n";
 		return (NULL);
+	}
 
 	collection = new Zombie[N];
-	if (collection == NULL)
+	if (collection == NULL) {
+		std::cerr << "[Error]: Failed to allocate the Zombie group!\n";
 		return (NULL);
+	}
 	for (int i = 0 ; i < N; i += 1)
 		collection[i].setName(name);
 
